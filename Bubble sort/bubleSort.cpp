49c63541ef157224ede61398a36116cb3206b4ce
AHMAD <57689837+ahmadner@users.noreply.github.com>
2022-04-15 06:53:14 -0400

    Update bubleSort.cpp

--- Bubble sort/bubleSort.cpp
+++ Bubble sort/bubleSort.cpp
@@ -1,50 +1,85 @@
+// bubble sorting array with flag
+// gcc version 10.2.1 20210110 (Debian 10.2.1-6)
+// github: @ahmadner
 #include <iostream>
 using namespace std;
 void insertVal(int arr[],int size);
-void switchVal(int arr[],int val1,int val2);
-void printArr(int arr[],int size);
+void swapvalue(int arr[],int val1,int val2);
+void printArray(int arr[],int size);
+int orderNumFunc(int);
+void ascendingOrderFunc(int arr[] ,int size);
+void descendingOrderFunc(int arr[] ,int size);
 int main(){
 
 int size;
-cout<<"enter array size : ";cin>>size;
+cout<<"[*] enter array size : ";cin>>size;
 int arr[size];
 insertVal(arr,size);
+int orderNum;
+cout<<"\n[*] orderNum:\n[1] Ascending orderNum\n[2] Descending orderNum\n\n=> ";
+cin>>orderNum;
 
-int flag;
-flag =size;
-for (int i=0;i<size;i++){
-	flag-=1;
-
-	for (int j=0;j<flag;j++){
-		if (arr[j]<arr[j+1])
-			switchVal(arr,j,j+1);
-	}
+if (orderNumFunc(orderNum) == 1)
+	ascendingOrderFunc(arr,size);
+else
+	descendingOrderFunc(arr,size);
+return 0;
 }
 
-printArr(arr,size);
-
-}
-
-
-void insertVal(int arr[],int len){
+void insertVal(int arr[],int size){
 	// insert data
-	for (int i=0;i<len;i++){
+	cout<<endl;
+	for (int i=0;i<size;i++){
 		cout<<i+1<<" => ";
 		cin>>arr[i];
 	}
-
 }
-
-void switchVal(int arr[],int val1,int val2){
-// switchVal(array name , value1 ,value2)
+void swapvalue(int arr[],int val1,int val2){
+// swapvalue(array name , value1 ,value2)
 	int tmp=arr[val1];
 	arr[val1]=arr[val2];
 	arr[val2]=tmp;
-
 }
-
-void printArr(int arr[],int len){
-	for (int i=0;i<len;i++){
+void printArray(int arr[],int size){
+	cout<<"\n[*]ordered array: ";
+	for (int i=0;i<size;i++){
 		cout<<arr[i]<<" ";
 	}
+	cout<<endl;
+}
+int orderNumFunc(int orderNum){
+	if (orderNum == 1 || orderNum == 2)
+		return orderNum;
+	else{
+		cout<<"[!] Choose between 1 or 2 only\n";
+		cout<<"\n=> ";
+		cin>>orderNum;
+		return orderNumFunc(orderNum);
+	}
+}
+void ascendingOrderFunc(int arr[],int size){
+
+	int flag;
+	flag =size;
+	for (int i=0;i<size;i++){
+		flag-=1;
+
+		for (int j=0;j<flag;j++)
+			if (arr[j] > arr[j+1])
+				swapvalue(arr,j,j+1);
+	}
+	printArray(arr,size);
+}
+void descendingOrderFunc(int arr[],int size){
+
+	int flag;
+	flag =size;
+	for (int i=0;i<size;i++){
+		flag-=1;
+
+		for (int j=0;j<flag;j++)
+			if (arr[j] < arr[j+1])
+				swapvalue(arr,j,j+1);
+	}
+	printArray(arr,size);
 }
